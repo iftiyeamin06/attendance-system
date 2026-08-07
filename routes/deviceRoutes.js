e8ff36f81112b8af518290ca92ce7b3e5ef3c223
IftiYeamin <iftiyeamin06@gmail.com>
2026-08-07 22:55:58 +0600

    fix: move my-ip route definition above auth middleware

--- routes/deviceRoutes.js
+++ routes/deviceRoutes.js
@@ -4,10 +4,11 @@ const router = express.Router();
 const authMiddleware = require('../middleware/auth');
 const deviceController = require('../controllers/deviceController');
 
+router.get('/my-ip', deviceController.myIp);
+
 router.use(authMiddleware);
 
 router.post('/register', deviceController.registerDevice);
 router.get('/status', deviceController.checkDeviceStatus);
-router.get('/my-ip', deviceController.myIp);
 
 module.exports = router;
